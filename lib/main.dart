0b936cc60e1cbcff7f30e0c20aef4b21c121acce
渡辺孝之 <na8esin.mpst@gmail.com>
2021-11-16 07:52:53 +0900

    riverpodで書き換え中

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,51 @@
-import 'dart:async';
-
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:artemis/artemis.dart';
 
 import 'graphql/big_query.dart';
-import 'graphql/simple_query.dart';
 
-Future<void> main() async {
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: MyBody(),
+    ),
+  ));
+}
+
+final bigQueryProvider = FutureProvider((ref) async {
   final client = ArtemisClient(
     'https://graphql-pokemon2.vercel.app',
   );
-
-  final simpleQuery = SimpleQueryQuery();
   final bigQuery = BigQueryQuery(variables: BigQueryArguments(quantity: 5));
 
-  final bigQuery2 = BigQueryQuery(variables: BigQueryArguments(quantity: 5));
-
-  print('Equality works: ${bigQuery == bigQuery2}');
-
-  final simpleQueryResponse = await client.execute(simpleQuery);
   final bigQueryResponse = await client.execute(bigQuery);
-  client.dispose();
 
-  print('Simple query response: ${simpleQueryResponse.data?.pokemon?.number}');
+  ref.onDispose(() {
+    client.dispose();
+  });
+
+  return bigQueryResponse.data?.pokemons;
 
   for (final pokemon in bigQueryResponse.data?.pokemons ?? []) {
     print('#${pokemon.number}: ${pokemon.name}');
+  }
+});
+
+class MyBody extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncPokemons = ref.watch(bigQueryProvider);
+    return ListView(children: [
+      asyncPokemons.when(
+          data: (pokemons) {
+            if (pokemons == null || pokemons.isEmpty) {
+              return const SizedBox.shrink();
+            } else{
+              return pokemons.map((e) => ListTile())
+            }
+          },
+          error: error,
+          loading: loading)
+    ]);
   }
 }
