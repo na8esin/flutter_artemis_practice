ee52fe000ec1ca46d392d93fe7db4e1f0706a5dd
渡辺孝之 <na8esin.mpst@gmail.com>
2021-11-18 21:55:32 +0900

    ポケモンたちをGridViewで表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:artemis/artemis.dart';
@@ -5,9 +6,11 @@ import 'package:artemis/artemis.dart';
 import 'graphql/big_query.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: MyBody(),
+  runApp(const ProviderScope(
+    child: MaterialApp(
+      home: Scaffold(
+        body: MyBody(),
+      ),
     ),
   ));
 }
@@ -16,7 +19,7 @@ final bigQueryProvider = FutureProvider((ref) async {
   final client = ArtemisClient(
     'https://graphql-pokemon2.vercel.app',
   );
-  final bigQuery = BigQueryQuery(variables: BigQueryArguments(quantity: 5));
+  final bigQuery = BigQueryQuery(variables: BigQueryArguments(quantity: 6));
 
   final bigQueryResponse = await client.execute(bigQuery);
 
@@ -25,27 +28,37 @@ final bigQueryProvider = FutureProvider((ref) async {
   });
 
   return bigQueryResponse.data?.pokemons;
-
-  for (final pokemon in bigQueryResponse.data?.pokemons ?? []) {
-    print('#${pokemon.number}: ${pokemon.name}');
-  }
 });
 
 class MyBody extends HookConsumerWidget {
+  const MyBody({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final asyncPokemons = ref.watch(bigQueryProvider);
-    return ListView(children: [
-      asyncPokemons.when(
-          data: (pokemons) {
-            if (pokemons == null || pokemons.isEmpty) {
-              return const SizedBox.shrink();
-            } else{
-              return pokemons.map((e) => ListTile())
-            }
-          },
-          error: error,
-          loading: loading)
-    ]);
+    return GridView.count(
+        crossAxisCount: 2,
+        children: asyncPokemons.when(
+            data: (pokemons) {
+              if (pokemons == null || pokemons.isEmpty) {
+                return [const SizedBox.shrink()];
+              } else {
+                return pokemons
+                    .map((e) => SizedBox(
+                          width: 300.0,
+                          height: 150.0,
+                          child: Center(
+                            child: CachedNetworkImage(
+                              placeholder: (context, url) =>
+                                  const CircularProgressIndicator(),
+                              imageUrl: e!.image!,
+                            ),
+                          ),
+                        ))
+                    .toList();
+              }
+            },
+            error: (e, s) => [Text(e.toString())],
+            loading: () => [const CircularProgressIndicator()]));
   }
 }
